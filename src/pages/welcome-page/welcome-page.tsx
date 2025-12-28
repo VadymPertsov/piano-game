7e74d240bb29a51a65b8a2024337bd7bfca92f2e
VadymPertsov <v.pertsov@quantumobile.com>
2025-12-28 16:38:12 +0200

    prettify ui in welcome page

--- src/pages/welcome-page/welcome-page.tsx
+++ src/pages/welcome-page/welcome-page.tsx
@@ -1,3 +1,23 @@
+import { Link } from 'react-router-dom'
+
+import { ROUTES } from '@src/shared/constants/routes'
+import { MaxContainerWrapper } from '@src/shared/ui/max-container-wrapper'
+
+import styles from './styles.module.scss'
+
 export const WelcomePage = () => {
-  return <div>WelcomePage</div>
+  return (
+    <MaxContainerWrapper className={styles.root}>
+      <h1 className={styles.title}>PianoMania</h1>
+      <div className={styles.actions}>
+        <Link to={ROUTES.play} className={styles.btn}>
+          Play
+        </Link>
+        <Link to={ROUTES.beatmaps} className={styles.btn}>
+          Browse Beatmaps
+        </Link>
+        <button className={styles.btn}>Settings</button>
+      </div>
+    </MaxContainerWrapper>
+  )
 }
